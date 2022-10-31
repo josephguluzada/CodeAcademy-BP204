049e64155086a89cacf136393ba363303e66d58d
josephguluzada <yquluzade98@gmail.com>
2022-10-31 17:47:30 +0400

    added loops tasks' solutions

---
+++ 31-10-2022/Loops/Loops/Loops/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1den 10a kimi olan reqemler arasinda cut olanlarin cemini tapan algorithm
+
+            //int sum = 0;
+            //int i = 1;
+
+            //while (i<10)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //    i++;
+            //}
+
+            //Console.WriteLine("Cem : " + sum);
+
+
+
+            // 1den 10a kimi olan reqemler arasinda tek olanlarin cemini tapan algorithm
+
+            //int sum = 0;
+
+            //for(int i = 1; i < 10; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+
+            //Console.WriteLine("Cem : " + sum);
+
+            //string name;
+            //do
+            //{
+            //    Console.WriteLine("Adiniz daxil edin:");
+            //    name = Console.ReadLine();
+
+            //} while (name.Length <= 4);
+
+            //int i = 1;
+
+            //while (i<10)
+            //{
+            //    if(i == 5)
+            //    {
+            //        Console.WriteLine("tapdim " +  i);
+            //        i++;
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+
+            int j = 1;
+
+
+            while (j<10)
+            {
+                if(j == 7)
+                {
+                    Console.WriteLine("Tapdim: " + j);
+                    break;
+                }
+                j++;
+            }
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 31-10-2022/LoopsInJs/index.js
@@ -0,0 +1,27 @@
+// 1den 10a kimi olan reqemler arasinda cut olanlarin cemini tapan algorithm
+
+// let sum = 0;
+// let i = 1;
+
+// while (i < 10) {
+//     if (i % 2 == 0) {
+//         sum += i;
+//     }
+//     i++;
+// }
+
+// console.log("Cem : " + sum);
+
+
+
+// 1den 10a kimi olan reqemler arasinda tek olanlarin cemini tapan algorithm
+
+// let sum = 0;
+
+// for (let i = 1; i < 10; i++) {
+//     if (i % 2 == 1) {
+//         sum += i;
+//     }
+// }
+
+// console.log("Cem : " + sum);
